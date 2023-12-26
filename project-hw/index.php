ee098d64e002f38f24941ff54bdc8450c3b1a83f
aarif <aarifhsn@gmail.com>
2023-12-26 19:36:23 +0600

    first commit

--- project-hw/index.php
+++ project-hw/index.php
@@ -6,37 +6,57 @@
     <title>Document</title>
 </head>
 <body>
-    
-<pre>
-    <?php
-        print_r(($_FILES));
-        if($_FILES['photo']) {
 
-            $photo_type = array('image/gif','image/jpg','image/jpe','image/jpeg','image/png');
-            
-            if(!in_array($_FILES['photo']['type'], $photo_type)) {
-                echo "File type not Supported";
-                exit;
-            }
-            $size = 1024*1024;
-            if($_FILES['photo']['size'] > $size) {
-                echo "File size must be less than 1MB";
-                exit;
-            }
-
-            $uploads_dir = 'uploads';
-            $photos_name = $_FILES["photo"]["tmp_name"];
-            $name = $_FILES["photo"]["name"];
-            move_uploaded_file($photos_name, "$uploads_dir/$name");
-        }
-    ?>
-</pre>
 
     <form action="" method="POST" enctype="multipart/form-data">
         <input type="file" name="photo[]" multiple>
         <input type="submit" name="submit" value="submit">
-
     </form>
+
+        
+<pre>
+    <?php
+        print_r(($_FILES));
+
+        
+        if (isset($_FILES) && !empty($_FILES)) {
+
+            // $uploads_dir = "uploads";
+            // $photos_name = $_FILES["photo"]["tmp_name"];
+            // $name = $_FILES["photo"]["name"];
+
+            // $photo_type = array("image/gif","image/jpg","image/jpe","image/jpeg","image/png");
+            
+            // if(!in_array($_FILES["photo"]["type"], $photo_type)) {
+            //     echo "File type not Supported";
+            //     exit;
+            // }
+            // $size = 1024*1024;
+            // if($_FILES["photo"]["size"] > $size) {
+            //     echo "File size must be less than 1MB";
+            //     exit;
+            // }   
+
+            foreach($_FILES['photo']['tmp_name'] as $key => $tmp_name) {
+                $file_name = $_FILES['photo']['name'][$key];
+                $file_size = $_FILES['photo']['size'][$key];
+                $file_tmp = $_FILES['photo']['tmp_name'][$key];
+                $file_type = $_FILES['photo']['type'][$key];
+
+           
+                if(move_uploaded_file($file_tmp, "uploads/" . $file_name)) {
+                
+                    echo "File Uploaded Successfully";
+                } else {
+                    "File Upload Failed";
+                }
+
+                
+            }
+           
+        }
+    ?>
+</pre>
 
 </body>
 </html>
---
+++ project-hw/uploads/practice.js
@@ -0,0 +1,51 @@
+// // add scripts to the document
+// // const headScripts = () => {
+// //     const scriptsElement = document.createElement("script");
+// //     scriptsElement.type= "text/javascript";
+// //     scriptsElement.src=  "script2.js";
+// //     document.head.appendChild(scriptsElement);
+// // }
+// //headScripts();
+
+// // add title in the head
+// document.title = "JS DOM Practice";
+
+// // Create a div element and add it within the document.
+// let container = document.createElement('div');
+
+// document.body.appendChild(container);
+
+// const headerArea = () => {
+//     let headerDiv = document.createElement('div');
+//     let headerLeftDiv = document.createElement('div');
+//     headerDiv.appendChild(headerLeftDiv);
+//     let headerRightDiv = document.createElement('div');
+//     headerDiv.appendChild(headerRightDiv);
+
+// }
+// let divElement = document.createElement('div');
+// let pElement = document.createElement('p');
+
+// let node = document.createTextNode('This is new.');
+
+// divElement.appendChild(pElement);
+
+// pElement.appendChild(node);
+
+
+// function addElement() {
+//   // create a new div element
+//   const newDiv = document.createElement("div");
+
+//   // and give it some content
+//   const newContent = document.createTextNode("Hi there and greetings!");
+
+//   // add the text node to the newly created div
+//   newDiv.appendChild(newContent);
+
+//   // add the newly created element and its content into the DOM
+//   //const currentDiv = document.getElementById("div1");
+//   //document.body.insertBefore(newDiv, currentDiv);
+// }
+
+// addElement();
